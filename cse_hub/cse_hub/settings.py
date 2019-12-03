5f071d3df14255e3015ff4f27b163880d7dcd335
harshraj22 <harshraj22aug@gmail.com>
2019-12-04 03:28:44 +0530

    added mathfilters to do arithmetics in templates

--- cse_hub/cse_hub/settings.py
+++ cse_hub/cse_hub/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'crispy_forms',
+    'mathfilters',
     'evaluation.apps.EvaluationConfig',
     'homepage.apps.HomepageConfig',
     'problems.apps.ProblemsConfig',
