be8bf00ae04a7aac160dd982072bbbe3a99008f1
harshraj22 <harshraj22aug@gmail.com>
2019-12-29 20:03:14 +0530

    added option to store and display TLE verdict on the proflie page

--- cse_hub/users/models.py
+++ cse_hub/users/models.py
@@ -6,6 +6,8 @@ class profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	problems_tried = models.IntegerField(default=0)
 	problems_solved = models.IntegerField(default=0)
+	problems_TLE = models.IntegerField(default=0)
+	problems_WA = models.IntegerField(default=0)
 
 	def __str__(self):
 		return self.user.username
