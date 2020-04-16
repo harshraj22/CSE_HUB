9bd97c23fe128b508dc9ba6cf5997075a856ee7a
harshraj22 <harshraj22aug@gmail.com>
2020-04-17 01:02:25 +0530

    added security layer to accept only allowed files

--- cse_hub/problems/forms.py
+++ cse_hub/problems/forms.py
@@ -1,10 +1,13 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, FileInput
 from .models import Problem, TestCase, Submissions
 
 class SubmitSolutionForm(ModelForm):
 	class Meta:
 		model = Submissions
 		fields = ['submission_code']
+		widgets = {
+			'submission_code': FileInput(attrs={'accept': '.py,.cpp'}),
+		}
 
 class ProblemForm(ModelForm):
 	class Meta:
@@ -15,6 +18,10 @@ class TestCaseForm(ModelForm):
 	class Meta:
 		model = TestCase
 		fields = ['testcase', 'solution', 'question']
+		widgets = {
+			'testcase': FileInput(attrs={'accept': '.txt'}),
+			'solution': FileInput(attrs={'accept': '.txt'}),
+		}
 
 	def __init__(self, *args, **kwargs):
 		# for current user, display only those problems which he/she has added
